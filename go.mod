124e9aef18539530fd92c846780ac41a3d63b70d
Maxsuel Marinho <maxsuelmarinho@gmail.com>
2019-03-08 20:44:02 -0300

    Add go mod; Formatting discord message; Change to use AlertManager structs

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/maxsuelmarinho/alertmanager-discord
+
+require github.com/prometheus/alertmanager v0.16.1
--- main.go
+++ main.go
@@ -6,9 +6,13 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"net/http/httputil"
 	"os"
 	"strings"
+
+	"github.com/prometheus/alertmanager/template"
 )
 
 type alertManAlert struct {
@@ -41,62 +45,137 @@ type alertManOut struct {
 	Version  string `json:"version"`
 }
 
-type discordOut struct {
-	Content string `json:"content"`
-	Name    string `json:"username"`
+type discordAuthor struct {
+	Name    string `json:"name"`
+	IconUrl string `json:"icon_url"`
+}
+
+type discordFooter struct {
+	Text string `json:"text"`
+}
+
+type discordField struct {
+	Name  string `json:"name"`
+	Value string `json:"value"`
+}
+
+type discordEmbed struct {
+	Title       string         `json:"title"`
+	Author      discordAuthor  `json:"author"`
+	Description string         `json:"description"`
+	Fields      []discordField `json:"fields"`
+	Color       int            `json:"color"`
+	Footer      discordFooter  `json:"footer"`
+}
+
+type discordRequest struct {
+	Content   string         `json:"content"`
+	Username  string         `json:"username"`
+	AvatarURL string         `json:"avatar_url"`
+	Embeds    []discordEmbed `json:"embeds"`
 }
 
 func main() {
-	webhookUrl := os.Getenv("DISCORD_WEBHOOK")
-	whURL := flag.String("webhook.url", webhookUrl, "")
+	webhookURL := os.Getenv("DISCORD_WEBHOOK")
+	whURL := flag.String("webhook.url", webhookURL, "")
 	flag.Parse()
 
-	if webhookUrl == "" && *whURL == "" {
-		fmt.Fprintf(os.Stderr, "error: environment variable DISCORD_WEBHOOK not found\n")
+	if webhookURL == "" && *whURL == "" {
+		log.Fatalf("error: environment variable DISCORD_WEBHOOK not found")
 		os.Exit(1)
 	}
 
-	fmt.Fprintf(os.Stdout, "info: Listening on 0.0.0.0:9094\n")
+	log.Printf("info: Listening on 0.0.0.0:9094")
 	http.ListenAndServe(":9094", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		printRequest(r)
+
 		b, err := ioutil.ReadAll(r.Body)
 		if err != nil {
 			panic(err)
 		}
 
-		amo := alertManOut{}
-		err = json.Unmarshal(b, &amo)
+		alertManagerNotification := template.Data{}
+		err = json.Unmarshal(b, &alertManagerNotification)
 		if err != nil {
 			panic(err)
 		}
 
-		groupedAlerts := make(map[string][]alertManAlert)
+		groupedAlerts := make(map[string][]template.Alert)
 
-		for _, alert := range amo.Alerts {
+		for _, alert := range alertManagerNotification.Alerts {
 			groupedAlerts[alert.Status] = append(groupedAlerts[alert.Status], alert)
 		}
 
 		for status, alerts := range groupedAlerts {
-			DO := discordOut{
-				Name: status,
+			request := discordRequest{
+				Username:  "Prometheus",
+				AvatarURL: "https://avatars1.githubusercontent.com/u/3380462?s=200&v=4",
+				Embeds:    make([]discordEmbed, 0),
 			}
 
-			Content := "```"
-			if amo.CommonAnnotations.Summary != "" {
-				Content = fmt.Sprintf(" === %s === \n```", amo.CommonAnnotations.Summary)
+			var iconUrl string
+			var color int
+			if strings.ToUpper(status) == "FIRING" {
+				iconUrl = "https://www.iconfinder.com/icons/116853/download/png/128"
+				color = 0
+			} else {
+				iconUrl = "https://www.iconfinder.com/icons/2682848/download/png/128"
+				color = 255
 			}
 
 			for _, alert := range alerts {
-				realname := alert.Labels["instance"]
-				if strings.Contains(realname, "localhost") && alert.Labels["exported_instance"] != "" {
-					realname = alert.Labels["exported_instance"]
+
+				embed := discordEmbed{
+					Author: discordAuthor{
+						Name:    strings.ToUpper(status),
+						IconUrl: iconUrl,
+					},
+					Title:       fmt.Sprintf("**%s**", alert.Annotations["summary"]),
+					Description: alert.Annotations["description"],
+					Color:       color,
+					Fields:      make([]discordField, 0),
+					Footer: discordFooter{
+						Text: "",
+					},
 				}
-				Content += fmt.Sprintf("[%s]: %s on %s\n%s\n\n", strings.ToUpper(status), alert.Labels["alertname"], realname, alert.Annotations.Description)
+
+				var value string
+				for k, v := range alert.Labels {
+					value += fmt.Sprintf("**%s:** %s\n", k, v)
+				}
+
+				field := discordField{
+					Name:  "Labels:",
+					Value: value,
+				}
+
+				embed.Fields = append(embed.Fields, field)
+
+				request.Embeds = append(request.Embeds, embed)
 			}
 
-			DO.Content = Content + "```"
-
-			DOD, _ := json.Marshal(DO)
-			http.Post(*whURL, "application/json", bytes.NewReader(DOD))
+			requestJSON, _ := json.Marshal(request)
+			response, err := http.Post(*whURL, "application/json", bytes.NewReader(requestJSON))
+			if err != nil {
+				log.Fatalf("failed to call discord webhook: %v", err)
+			}
+			var body string
+			bodyBytes, err := ioutil.ReadAll(response.Body)
+			response.Body.Close()
+			if err != nil {
+				body = fmt.Sprintf("failed read discord webhook response body: %v", err)
+			} else {
+				body = string(bodyBytes)
+			}
+			log.Printf("Discord webhook response: [httpStatus: %d; body: %s]", response.StatusCode, body)
 		}
 	}))
+}
+
+func printRequest(r *http.Request) {
+	requestDump, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		fmt.Println(err)
+	}
+	log.Printf("Request received: %s", string(requestDump))
 }
